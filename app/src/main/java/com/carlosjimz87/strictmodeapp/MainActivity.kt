4c79464bce6db9cc49b24a1bb8a969996c18a70b
carlosjimz87 <carlosjimz87@gmail.com>
2022-04-16 00:12:51 +0200

    being compliant with StrictMode

--- app/src/main/java/com/carlosjimz87/strictmodeapp/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/strictmodeapp/MainActivity.kt
@@ -4,23 +4,31 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import java.lang.Thread.MAX_PRIORITY
 
 const val MY_PREf = "MY_PREF"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var sharedPrefs: SharedPreferences
+    private var sharedPrefs: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        sharedPrefs= PreferenceManager.getDefaultSharedPreferences(this)
+        val thread =Thread{
+            sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
+        }
+
+        thread.priority = MAX_PRIORITY
+        thread.start()
 
         backgroundAccessPrefs()
+
     }
 
-    fun backgroundAccessPrefs(){
-        Thread{
-            sharedPrefs.getInt(MY_PREf,-1)
+    private fun backgroundAccessPrefs() {
+        Thread {
+            sharedPrefs?.getInt(MY_PREf, -1)
         }.start()
+
     }
 }
