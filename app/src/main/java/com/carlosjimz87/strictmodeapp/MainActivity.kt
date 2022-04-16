cd08d241c7f0bcf26cbb497d30a361d51a86f4ab
carlosjimz87 <carlosjimz87@gmail.com>
2022-04-17 00:42:16 +0200

    change to DataStore (thread safe with StrictMode)

--- app/src/main/java/com/carlosjimz87/strictmodeapp/MainActivity.kt
+++ app/src/main/java/com/carlosjimz87/strictmodeapp/MainActivity.kt
@@ -1,30 +1,54 @@
 package com.carlosjimz87.strictmodeapp
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-const val MY_PREf = "MY_PREF"
 
 class MainActivity : AppCompatActivity() {
-    private var sharedPrefs: SharedPreferences? = null
+    private val dataStore by preferencesDataStore(name = "settings")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread {
-            sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
-        }.start()
 
-        backgroundAccessPrefs()
+        // write data store value
+        lifecycleScope.launch {
+            save("KEY", "HELLO")
+        }
+
+        // write data store value at click
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                val text = read("KEY", "")
+
+                withContext(Dispatchers.Main){
+                    textView.text = text
+                }
+            }
+
+        }
 
     }
 
-    private fun backgroundAccessPrefs() {
-        Thread {
-            sharedPrefs?.getInt(MY_PREf, -1)
-        }.start()
+    private suspend fun save(key: String, value: String) {
+        val dataStoreKey = stringPreferencesKey(key)
+        dataStore.edit { settings ->
+            settings[dataStoreKey] = value
+        }
+    }
 
+    private suspend fun read(key: String, default: String): String {
+        val dataStoreKey = stringPreferencesKey(key)
+        val preferences = dataStore.data.first()
+        return preferences[dataStoreKey] ?: default
     }
 }
